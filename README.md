7b5aeeb1ad15c37ba2a56a2247903053e7babda1
RafaelDavisH <thinkprintgrafx@gmail.com>
2017-09-28 21:32:16 -0700

    add more instances(movies), update fresh_tomatoes.py - webpage title, comments added to instances

--- README.md
+++ README.md
@@ -7,7 +7,11 @@ A simple and dynamic project website that showcase a few of my favorite movie tr
 - Poster image
 - Title
 - Storyline
-- Trailer youtube video.   
+- Trailer youtube video. 
+
+**You'll be running `entertainment_center.py` script to load the website,
+but first:**
+
 
 ### You'll Need:
 
@@ -16,3 +20,54 @@ A simple and dynamic project website that showcase a few of my favorite movie tr
 `media.py`
 
 `entertainment_center.py`
+
+
+### Fresh Tomatoes
+
+The `fresh_tomatoes.py` contains the `open_movies_page()` function that will
+take in the list of movies and generate an HTML file including its content,
+producing a website to showcase my favorite movie trailers.
+
+
+### Media
+
+`media.py` contains class called `Movie()`, which contains the `__init__`
+constructor, four instance variables and an instance method.
+
+*`__int__` constructor*
+
+This constructor has five arguments, including `self`, which are:
+
+1. `self`
+
+2. `movie_title`
+
+3. `movie_storyline`
+
+4. `poster_image`
+
+5. `trailer_youtube`
+
+*Instance Variables:*
+
+1. `title`
+
+2. `stotyline`
+
+3. `poster_image_url`
+
+4. `trailer_youtube_url`
+
+*Instance Method*
+
+`show_trailer()` is the method that will show the movie youtube trailer in a
+new tab.
+
+### Entertainment Center
+
+`entertainment_center.py` contains the instances of `Movie()`. The movie
+objects calls the constructor `media.Movie()`to instantiate movie objects. Each
+movie has its own custom data structure defined by the move class and
+constructor, and are stored in a list data structure. This list of movie
+is what the `open_movies_page()` function needs as input in order to build the
+HTML file, to display the website.
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,7 @@
+#!/usr/bin/python2.7
+
 import fresh_tomatoes
 import media
-
-# Bourne Indentity: movie title, storyline, poster image and movie trailer
-the_bourne_identity = media.Movie(
-    "The Bourne Identity", """An amnesiac attempting to discover
-    his true identity amidist a clandestine conspiracy within
-    the Central Intelligence Agency.""",
-    "https://vignette2.wikia.nocookie.net/bourne/images/d/d0/Identity_poster_01.jpg/revision/latest?cb=20120223045603",
-    "https://youtu.be/cD-uQreIwEk"
-    )
-
 
 
 # Italian Job: movie title, storyline, poster image and movie trailer
@@ -25,7 +17,7 @@ the_italian_job = media.Movie(
 the_chronicles_of_narnia = media.Movie(
     "The Voyage of the Dawn Treader",
     "Three Narnian years after the events of Prince Caspian.",
-    "https://upload.wikimedia.org/wikipedia/en/d/d4/The_Voyage_of_the_Dawn_Treader_poster.jpg",
+    "https://goo.gl/PMzJ1P",
     "https://www.youtube.com/watch?v=OubkxGzVuxU")
 
 # Saving Private Ryan: movie title, storyline, poster imag and movie trailer
@@ -33,18 +25,58 @@ saving_private_ryan = media.Movie(
     "Saving Private Ryan",
     """A 1998 American epic war drama film set during the Invasion
     of Normandy in World War II.""",
-    "https://upload.wikimedia.org/wikipedia/en/a/ac/Saving_Private_Ryan_poster.jpg",
+    "https://goo.gl/QSjRQo",
     "https://www.youtube.com/watch?v=RYExstiQlLc"
     )
 
-# Secret Life of Walter Mitty: movie title, storyline, poster imag and movie trailer
-the_secret_life_of_walter_mitty = media.Movie(
-    "The Secret Life of Walter Mitty",
-    """Walter Mitty is a negative assest manager at Life magazine
-    who daydreams of adventures.""",
-    "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Secret_Life_of_Walter_Mitty_poster.jpg",
-    "https://www.youtube.com/watch?v=eoILTydXrTY"
+# The Bourne Indentity: movie title, storyline, poster image and movie trailer
+the_bourne_identity = media.Movie(
+    "The Bourne Identity", """An amnesiac attempting to discover
+    his true identity amidist a clandestine conspiracy within
+    the Central Intelligence Agency.""",
+    "https://goo.gl/Zdbhw4",
+    "https://youtu.be/cD-uQreIwEk"
     )
+
+# The Bourne Supremacy: movie title, storyline, poster image and movie trailer
+the_bourne_supremacy = media.Movie(
+    "The Bourne Supremacy",
+    """Jason focuses in learning more of his past as he is
+    once more enveloped in a conspiracy invoving the CIA and Operation
+    Treadstone.""",
+    "https://goo.gl/uJaHHr",
+    "https://youtu.be/Y-HqyyfBbSo"
+  )
+
+# The Bourne Ultimatum: movie title, storyline, poster image and movie trailer
+the_bourne_ultimatum = media.Movie(
+    "The Bourne Ultimatum",
+    """Jason continues his search for information about his past before he was
+    part of Operation Treadstone and becomes a target of a similar assassin
+    program. """,
+    "https://goo.gl/W1Ue6C",
+    "https://youtu.be/ZT2ZxjUjSo0"
+)
+
+# The Bourne legacy: movie title, storyline, poster image and movie trailer
+the_bourne_legacy = media.Movie(
+    "The Bourne Legacy",
+    """In The Bourne Legacy, Aaron Cross is a member of a black ops program
+    called Operation Outcome whose subjects are genetically enhanced. He must
+    run for his life.""",
+    "https://goo.gl/6okU8T",
+    "https://youtu.be/cdtUdEoE-Q4"
+)
+
+# Jason Bourne: movie title, storyline, poster image and movie trailer
+jason_bourne = media.Movie(
+    "Jason Bourne",
+    """Bourne remains on the run from CIA hit squads as he tries to uncover
+    hidden truths about his father. CIA director Robert Dewey orders the CIA
+    head of cyber-security Heather Lee to hunt him down.""",
+    "https://goo.gl/ZBDFr2",
+    "https://youtu.be/v71ce1Dqqns"
+)
 
 # Dark Knight Rises: movie title, storyline, poster imag and movie trailer
 the_dark_knight_rises = media.Movie(
@@ -52,17 +84,51 @@ the_dark_knight_rises = media.Movie(
     """Eight years after the events of The Dark Knight, merciless
     revolutionary Bane forces an older Bruce Wayne to resume his
     role as Batman and save Gotham City from nuclear destruction.""",
-    "https://upload.wikimedia.org/wikipedia/en/8/83/Dark_knight_rises_poster.jpg",
+    "https://goo.gl/fwGxHX",
     "https://www.youtube.com/watch?v=ay-Xg1oTeAs"
     )
 
+# Jurassic Park: movie title, storyline, poster image and movie trailer
+jurassic_park = media.Movie(
+    "Jurassic Park",
+    """Industrialist John Hammond and his bioengineering company, InGen, have
+    created a theme park called Jurassic Park on Isla Nublar, a Costa Rican
+    Island, pupulated with cloned dinosaurs.""",
+    "https://goo.gl/jyE4mL",
+    "https://youtu.be/QWBKEmWWL38"
+)
+
+# The Lost World: movie title, storyline, postyer image and movie trailer
+the_lost_world = media.Movie(
+    "The Lost World",
+    """Four years after the events of the Jurassic Park dinosaurs roaming free
+    in the Isla Nublar. An expedition its set to stop of the capturing of the dinosaurs.""",
+    "https://goo.gl/yDsNqa",
+    "https://youtu.be/RxrvaneULkE"
+)
+
+# Secret Life of Walter Mitty: movie title, storyline, poster imag and movie trailer
+the_secret_life_of_walter_mitty = media.Movie(
+    "The Secret Life of Walter Mitty",
+    """Walter Mitty is a negative assest manager at Life magazine
+    who daydreams of adventures.""",
+    "https://goo.gl/MiWNye",
+    "https://www.youtube.com/watch?v=eoILTydXrTY"
+    )
+
 # Set movies to pass to the media file
-movies = [the_bourne_identity,
-          the_italian_job,
+movies = [the_italian_job,
           the_chronicles_of_narnia,
           saving_private_ryan,
+          the_bourne_identity,
+          the_bourne_supremacy,
+          the_bourne_ultimatum,
+          the_bourne_legacy,
+          the_dark_knight_rises,
+          jason_bourne,
+          jurassic_park,
+          the_lost_world,
           the_secret_life_of_walter_mitty,
-          the_dark_knight_rises
           ]
 
 # Open HTML file in webbrowser via the fresh_tomatoes.py file
